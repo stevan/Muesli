26a85742bd68084129f75fa0c05b275253a0c590
Stevan Little <stevan.little@booking.com>
2015-08-24 11:04:49 +0200

    use the Util::Devel package

--- lib/Muesli/Encoder.pm
+++ lib/Muesli/Encoder.pm
@@ -10,6 +10,7 @@ use Scalar::Util qw[
     looks_like_number 
 ];
 
+use Muesli::Util::Devel;
 use Muesli::Util::Converters;
 use Muesli::Util::Constants;
 
@@ -22,34 +23,46 @@ sub encode {
     my @bytes = (MAGIC_HEADER);
 
     if ( not defined $data ) {
+        LOG(INFO => 'Encoding UNDEF') if DEBUG;
         push @bytes => encode_undef( $data );
     }
     elsif ( ref $data ) {
+        LOG(INFO => 'Encoding a ref') if DEBUG;
 
         die '[PANIC] Currently blessed items are not supported'
             if blessed $data;
 
         my $type = reftype $data;
         if ( $type eq 'ARRAY' ) {
+            LOG(INFO => 'Encoding ARRAY ref') if DEBUG;
             push @bytes => encode_array( $data );
         }
         elsif ( $type eq 'HASH' ) {
+            LOG(INFO => 'Encoding HASH ref') if DEBUG;
             push @bytes => encode_hash( $data );
         }
         else {
+            LOG(FATAL => 'Attempting to encode an unsupported reftype %s', $type) if DEBUG;
             die '[PANIC] Currently items of type(' . $type . ') are not supported'
         }
     }
     else { 
+        LOG(INFO => 'Encoding a non-ref') if DEBUG;
+
         if ( looks_like_number( $data ) ) {
+            LOG(INFO => 'Encoding a number') if DEBUG;
+
             if ( B::svref_2object( \$data )->isa('B::NV') ) { # is a float?
+                LOG(INFO => 'Encoding FLOAT') if DEBUG;
                 push @bytes => encode_float( $data );
             }
             else {
+                LOG(INFO => 'Encoding INT') if DEBUG;
                 push @bytes => encode_int( $data );
             }
         }
         else {
+            LOG(INFO => 'Encoding STRING') if DEBUG;
             push @bytes => encode_string( $data );
         }
     }
